08371c60659111d78a53f671dc9e1d913472f8d1
jane__ <janecching@gmail.com>
2022-09-23 09:13:44 -0600

    education and work section css

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -3,11 +3,28 @@
 var li_items = document.querySelectorAll(".accordion_wrap ul li");
 var ul = document.querySelector(".accordion_wrap ul");
 
-li_items.forEach(function(item){
-	item.addEventListener("click", function(){
-		li_items.forEach(function(item){
-			item.classList.remove("active");
-		})
-		item.classList.add("active");
-	});
+li_items.forEach(function(item) {
+    item.addEventListener("click", function() {
+        li_items.forEach(function(item) {
+            item.classList.remove("active");
+        })
+        item.classList.add("active");
+    });
 });
+
+// work
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
