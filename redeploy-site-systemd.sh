cd38f3ea632559fb016b8009fc0b81d3b3cb3e43
jane__ <janecching@gmail.com>
2022-11-05 05:31:03 +0800

    Add automated redeployment script using systemd

---
+++ redeploy-site-systemd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd MLH_SRE_Portfolio
+git fetch && git reset origin/main --hard
+python -m venv python3-virtualenv
+source python3-virtualenv/bin/activate
+pip install -r requirements.txt
+systemctl daemon-reload
+systemctl restart myportfolio
+systemctl status myportfolio
